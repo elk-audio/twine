38ffce0f672fbf9c4652e0487337ca93b8d42d76
Gustav Andersson <gustav.andersson@mindmusiclabs.com>
2018-07-13 17:01:58 +0200

    Adding lots of statistics to stress test

--- test/stresstest/stresstest.cpp
+++ test/stresstest/stresstest.cpp
@@ -45,8 +45,9 @@ constexpr float co_b0 = (1.0f - w0_cos) / 2.0f * norm;;
 constexpr float co_b1 = (1 - w0_cos) * norm;
 constexpr float co_b2 = co_b0;
 
-typedef std::array<float, 128> AudioBuffer;
-typedef std::array<float, 2> FilterRegister;
+using AudioBuffer = std::array<float, 128>;
+using FilterRegister = std::array<float, 2>;
+using TimeStamp = std::chrono::nanoseconds;
 
 inline void set_flush_denormals_to_zero()
 {
@@ -70,16 +71,20 @@ struct ProcessData
 {
     AudioBuffer buffer;
     FilterRegister mem;
+    TimeStamp start_time;
+    TimeStamp end_time;
 };
 
 void worker_function(void* data)
 {
     auto process_data = reinterpret_cast<ProcessData*>(data);
+    process_data->start_time = twine::current_rt_time();
     int iters = MAX_LOAD;
     for (int i = 0; i < iters; ++i)
     {
         process_filter(process_data->buffer, process_data->mem);
     }
+    process_data->end_time = twine::current_rt_time();
 }
 
 std::string to_error_string(twine::WorkerPoolStatus status)
@@ -122,15 +127,16 @@ void xenomai_thread_init()
 #endif
 
 
-std::tuple<int, int, int, bool> parse_opts(int argc, char** argv)
+std::tuple<int, int, int, bool, bool> parse_opts(int argc, char** argv)
 {
     int workers = DEFAULT_WORKERS;
     int cores = DEFAULT_CORES;
     int iters = DEFAULT_ITERATIONS;
     bool xenomai = false;
+    bool print_timings = false;
     char c;
 
-    while ((c = getopt(argc, argv, "w:c:i:x")) != -1)
+    while ((c = getopt(argc, argv, "w:c:i:xt")) != -1)
     {
         switch (c)
         {
@@ -143,6 +149,9 @@ std::tuple<int, int, int, bool> parse_opts(int argc, char** argv)
             case 'i':
                 iters = atoi(optarg);
                 break;
+            case 't':
+                print_timings = true;
+                break;
             case 'x':
                 if (!xenomai)
                 {
@@ -151,30 +160,95 @@ std::tuple<int, int, int, bool> parse_opts(int argc, char** argv)
                 }
                 break;
             case '?':
-                std::cout << "Options are: -w[n of worker threads], -c[n of cores], -i[n of iterations], -x - use xenomai threads" << std::endl;
+                std::cout << "Options are: -w[n of worker threads], -c[n of cores], -i[n of iterations], -x - use xenomai threads, -t - print timings for each iteration" << std::endl;
                 [[fallthrough]]
             default:
                 abort();
         }
     }
-    return std::make_tuple(workers, cores, iters, xenomai);
+    return std::make_tuple(workers, cores, iters, xenomai, print_timings);
+}
+
+
+void print_timings(const std::vector<ProcessData>* data, int iter, bool xenomai, TimeStamp start_time, TimeStamp end_time)
+{
+    static float min_time{10000000};
+    static float max_time{0};
+    static float mean_time{0};
+
+    float current_total = (end_time - start_time).count() / 1000.0f;
+    max_time = std::max(max_time, current_total);
+    min_time = std::min(min_time, current_total);
+    mean_time = (4.0f * mean_time + current_total) / 5;
+
+    if (xenomai)
+    {
+#ifdef TWINE_BUILD_WITH_XENOMAI
+        __cobalt_printf("Iteration: %i, total time: %.1f us, avg: %.1f us, min: %.1f us, max: %.1f us\n", iter, current_total, mean_time, min_time, max_time);
+#endif
+    }
+    else
+    {
+        std::cout << "Iteration: " << iter << ", total time: " << current_total << " us, avg: " << mean_time << " us, min: " << min_time << " us, max: "<< max_time << " us\n";
+    }
+    int id = 0;
+    for(const auto& w : *data)
+    {
+        TimeStamp process_time = (w.end_time - w.start_time) / 1000;
+        TimeStamp offset_time = (w.start_time - start_time) / 1000;
+        if (xenomai)
+        {
+#ifdef TWINE_BUILD_WITH_XENOMAI
+            __cobalt_printf("Worker: %i, start offset: %i us, total: %i us\n", id, offset_time.count(), process_time.count());
+#endif
+        }
+        else
+        {
+            std::cout << "Worker: " << id << ", start offset: " << offset_time.count() << " us, total: " << process_time.count() << " us" << std::endl;
+        }
+        id++;
+    }
+
+}
+
+void print_iterations(int iter, bool xenomai)
+{
+    if ((iter + 1) % 10 == 0)
+    {
+        if (xenomai)
+        {
+#ifdef TWINE_BUILD_WITH_XENOMAI
+            __cobalt_printf("\rIterations: %i", iter);
+#endif
+        }
+        else
+        {
+            std::cout << "\rIterations: " << iter;
+            std::cout.flush();
+        }
+    }
 }
 
 void* run_stress_test(void* data)
 {
     set_flush_denormals_to_zero();
-    auto [pool, iters, xenomai] = *(reinterpret_cast<std::tuple<twine::WorkerPool*, int, bool>*>(data));
+    auto [pool, process_data, iters, xenomai, timings] = *(reinterpret_cast<std::tuple<twine::WorkerPool*, const std::vector<ProcessData>*, int, bool, bool>*>(data));
     for (int i = 0; i < iters; ++i)
     {
+        auto start_time = twine::current_rt_time();
         pool->wait_for_workers_idle();
         // Run all workers
         pool->wakeup_workers();
+        auto end_time = twine::current_rt_time();
         if ((i + 1) % 10 == 0)
         {
+            if (!timings)
+            {
+                print_iterations(i, xenomai);
+            }
             if (xenomai)
             {
 #ifdef TWINE_BUILD_WITH_XENOMAI
-                __cobalt_printf("\rIterations: %i", i);
                 /* Since xenomai runs with realtime priority, we can't max out cpu usage as
                  * that would starve the linux kernel, so we leave a time slice for it here */
                 timespec t;
@@ -183,11 +257,10 @@ void* run_stress_test(void* data)
                 __cobalt_nanosleep(&t, nullptr);
 #endif
             }
-            else
-            {
-                std::cout << "\rIterations: " << i;
-                std::cout.flush();
-            }
+        }
+        if (timings)
+        {
+            print_timings(process_data, i, xenomai, start_time, end_time);
         }
     }
     return nullptr;
@@ -197,7 +270,7 @@ void run_stress_test_in_xenomai_thread(void* data)
 {
 #ifdef TWINE_BUILD_WITH_XENOMAI
         /* Threadpool must be controlled from another xenomai thread */
-        struct sched_param rt_params = { .sched_priority = 70 };
+        struct sched_param rt_params = { .sched_priority = 80 };
         pthread_attr_t task_attributes;
         __cobalt_pthread_attr_init(&task_attributes);
 
@@ -219,7 +292,7 @@ void run_stress_test_in_xenomai_thread(void* data)
 
 int main(int argc, char **argv)
 {
-    auto [workers, cores, iters, xenomai] = parse_opts(argc, argv);
+    auto [workers, cores, iters, xenomai, timings] = parse_opts(argc, argv);
 
     std::vector<ProcessData> data;
     data.reserve(DEFAULT_ITERATIONS);
@@ -246,7 +319,7 @@ int main(int argc, char **argv)
             return -1;
         }
     }
-    auto test_data = std::make_tuple(worker_pool.get(), iters, xenomai);
+    auto test_data = std::make_tuple(worker_pool.get(), &data, iters, xenomai, timings);
     if (xenomai)
     {
         run_stress_test_in_xenomai_thread(&test_data);
