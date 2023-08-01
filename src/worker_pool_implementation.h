ee22158586c3fc260ac99b6197bc9d9b1883e2b7
Baptiste Le Goff <baptiste@meteaure.com>
2023-08-01 09:53:43 +0200

    fix compilation on windows

--- src/worker_pool_implementation.h
+++ src/worker_pool_implementation.h
@@ -437,7 +437,7 @@ public:
         pthread_attr_setschedpolicy(&task_attributes, SCHED_FIFO);
         pthread_attr_setschedparam(&task_attributes, &rt_params);
         auto res = 0;
-#ifndef __APPLE__
+#if !defined __APPLE__ && !defined _WIN32 
         cpu_set_t cpus;
         CPU_ZERO(&cpus);
         CPU_SET(cpu_id, &cpus);
